354f8083d82c8f39bf397ad0df3c7423ab1cfa69
kdh <letitbe1334@gmail.com>
2023-06-29 10:07:00 +0900

    .

--- src/main/java/biz/riman/erp/batch/TestErpApiApplication.java
+++ src/main/java/biz/riman/erp/batch/TestErpApiApplication.java
@@ -1,17 +1,47 @@
 package biz.riman.erp.batch;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 @EnableBatchProcessing
 public class TestErpApiApplication {
+    private static final Logger logger = LoggerFactory.getLogger(TestErpApiApplication.class);
 
-	public static void main(String[] args) {
-	    SpringApplication.run(TestErpApiApplication.class, args);
+	public static void main(String[] args) throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
+//	    SpringApplication.run(TestErpApiApplication.class, args);
 //		final var context = SpringApplication.run(TestErpApiApplication.class, args);
 //		System.exit(SpringApplication.exit(context));
+        
+
+        SpringApplication app = new SpringApplication(TestErpApiApplication.class);
+        app.setWebApplicationType(WebApplicationType.NONE);
+        ConfigurableApplicationContext ctx = app.run(args);
+        JobLauncher jobLauncher = ctx.getBean(JobLauncher.class);
+
+        if (args.length > 0) {
+            Job job = ctx.getBean(args[0], Job.class);
+            jobLauncher.run(job, new JobParameters());
+        } else {
+            logger.error("배치 잡 이름을 입력해 주세요. EX) {}", "java -jar app.jar updateTop100CoinPriceJob");
+        }
+
+        System.exit(0);
 	}
 
 }
