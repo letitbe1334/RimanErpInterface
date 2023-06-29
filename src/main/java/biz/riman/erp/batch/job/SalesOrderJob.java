0b6bbfa3095758810034704dc4983ec100753ac7
kdh <letitbe1334@gmail.com>
2023-06-29 12:39:11 +0900

    .

--- src/main/java/biz/riman/erp/batch/job/SalesOrderJob.java
+++ src/main/java/biz/riman/erp/batch/job/SalesOrderJob.java
@@ -1,15 +1,11 @@
 package biz.riman.erp.batch.job;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,23 +17,17 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
-@EnableBatchProcessing
 public class SalesOrderJob {
-    private static final Logger logger = LoggerFactory.getLogger(SalesOrderJob.class);
-    
-    @Autowired 
-    public JobBuilderFactory jobBuilderFactory;
-    @Autowired 
-    public StepBuilderFactory stepBuilderFactory;
-    @Autowired 
-    public BTPConnectionService service;
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final BTPConnectionService service;
 
     public static final String JOB_NAME = "order.SalesBatch";
     public static final String STEP_NAME = ".syncStep";
 
     @Bean(name = JOB_NAME)
     public Job job() {
-        logger.info("## {} Job 실행 ##", JOB_NAME);
+        log.info("## {} Job 실행 ##", JOB_NAME);
         return jobBuilderFactory.get(JOB_NAME)
                 .start(syncStep())
                 .listener(new JobListener())
@@ -47,11 +37,11 @@ public class SalesOrderJob {
     @Bean(name = JOB_NAME + STEP_NAME)
     @JobScope
     public Step syncStep() {
-        logger.info("## {} - {} Step 실행 ##", JOB_NAME, STEP_NAME);
+        log.info("## {} - {} Step 실행 ##", JOB_NAME, STEP_NAME);
         return stepBuilderFactory.get(JOB_NAME + STEP_NAME)
                 .tasklet((contribution, chunkContext) -> {
                     String response = service.BTPConnectionSalesOrder();
-                    logger.info("## response : {}", response);
+                    log.info("## response : {}", response);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
